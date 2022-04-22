5979b079a3f68ca06c77e1b9cc3c18befd34231f
Your Name <you@example.com>
2022-04-22 18:11:29 +0900

    アイテムを説明を追加

--- Classes/Item.php
+++ Classes/Item.php
@@ -4,19 +4,22 @@
 
   abstract class Item{
 
-    public $explain_effect
-    =
-    [ "kinoko" => "サイコロを2つふることができる。",
-      "wana" => "今いるマスに罠を仕掛けることができる。",
-      "teleport" => "相手と居場所を交換する",
-    ];
+    static function printEffect($items){
+      $explain_effect
+      =
+      [ "kinoko" => "サイコロを2つふることができる。",
+        "wana" => "今いるマスに罠を仕掛けることができる。",
+        "teleport" => "相手と居場所を交換する",
+        "roto" => "いいことが起こるかも",
+      ];
 
-
-    function __construct(){
-
+      foreach($items as $key => $value){
+        echo $value . ":". $explain_effect[$value] . "\n";
+      }
     }
 
     static function useItem($name,$player="",$board="",$players=""){
+      //ひたすらここにアイテムの効果を書いていくいつか分別が必要
 
       if($name == "kinoko"){
         $dice1 = Dice::rollDice();
@@ -46,6 +49,27 @@
         $player->standing_point = $players[$number]->standing_point;
         $players[$number]->standing_point = $tmp;
         return;
+      }
+
+      if($name == "roto"){
+        echo "さぁ、サイコロを降りなさい！\n";
+        echo "でた目が1か6なら以下の効果が得られる！\n";
+        echo "1 => 全ての人の居場所を入れ替えちゃう。\n";
+        echo "6 => みんな振り出しになる\n";
+        $dice = Dice::rollDice();
+        sleep(1);
+        echo $dice . "\n\n";
+        sleep(1);
+        if($dice == 1){
+          echo "全ての人の居場所がランダムに入れ替わる！\n";
+          return ;
+        }
+        if($dice == 6){
+          echo "みんな振り出しになった！\n";
+          return;
+        }
+        echo "ハズレ！ターン終了\n";
+        return ;
       }
     }
 
--- Classes/Player.php
+++ Classes/Player.php
@@ -7,7 +7,7 @@
 
     public $name;
     public $standing_point;
-    public $items = ["kinoko","wana","teleport"];
+    public $items = ["kinoko","wana","teleport","roto"];
     public $special_skill;
 
     function __construct($name){
@@ -15,21 +15,16 @@
       $this->standing_point = 0;
     }
 
-    function goForward($n){
-      $this->standing_point += $n;
-    }
-
-    function goBackward($n){
-      $this->standing_point -= $n;
-    }
-
     function startCommand($dice,$board,$players){
       echo "\n" . $this->name . "さんの番です。";
       echo "コマンドを決めてください\n";
       while( true ){
+
         echo "サイコロ -> 1 , アイテム -> 2 , 必殺技 -> 3 \n";
+
         $selected_number = 0;
         while(!($selected_number == 1 || $selected_number == 2 || $selected_number == 3)){
+  
           echo "数字の1か2か3で入力してください\n";   
           $selected_number = fgets(STDIN);
 
@@ -41,25 +36,35 @@
           }
 
           if ($selected_number == 2){
-            echo "どのアイテムを使いますか？アイテムを使わない場合は、bを押してください。\n";
+            echo "どのアイテムを使いますか？アイテムの詳細を見るには9を押してください。\n";
             foreach($this->items as $key => $value){
               echo $value . "を使う => " . $key . "を入力\n";
             }
             $item_number = fgets(STDIN);
             if(!(is_numeric($item_number))){ break; }
             $item_number = (int) $item_number;
-            $item_name = $this->items[$item_number];
+            if($item_number == 9){ Item::printEffect($this->items); break;}
 
+            $item_name = $this->items[$item_number];
             if($item_name){
               echo $item_name . "を使った。\n";
               $number = Item::useItem($item_name,$this,$board,$players);
               unset($this->items[$item_number]);
-              //ここでアイテム毎のコマンドを実行させて、進む数字を返す。数字を返さなければ終わらせる。
               return $number;
             }
           }
+
         }
       }
+    }
+
+
+    function goForward($n){
+      $this->standing_point += $n;
+    }
+
+    function goBackward($n){
+      $this->standing_point -= $n;
     }
 
     function goOrBack($dice_number, $goal_point){
@@ -101,12 +106,11 @@
       if(is_string($number_on_standing_point)){
         if ($number_on_standing_point == "穴"){
           echo "振り出しに戻ってしまった！\n";
+          $this->standing_point = 0;
         }
         if ($number_on_standing_point == "アイテム"){
           echo "アイテムがもらえる\n";
-        }
-        if ($number_on_standing_point == "休み"){
-          echo "滑って転んで一回休みになった！\n";
+          $this->items += "kinoko";
         }
       }
 
